b07c799465fe18e6edfc069379573c4fed27b33f
DavidKal29 <davidkalmuk65@gmail.com>
2025-11-27 12:55:54 +0100

    Creamos en types, la interfaz Animal, y también, el metodo para obtener los RandomAnimals, que serán varios animales que aparecerán justo al inicio de la web, los cuales serán aleatorios. También, añadimos todas las imagenes de los animales

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,52 @@
 'use client'
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import {Animal} from "./types/animal"
 
 export default function Home() {
+
+  const [randomAnimals, setRandomAnimals] = useState<Animal[] | []>([])
+
+  const getRandomAnimals = ()=>{
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/animals/getRandomAnimals`,{
+      method:'GET',
+      credentials:'include'
+    })
+      .then(res=>res.json())
+      .then(data=>{
+        console.log(data);
+        
+        if (data.success) {
+          setRandomAnimals(data.animals)
+        }
+      
+      })
+      .catch(error=>{
+        console.log(error);
+        toast.error('Error al enviar los datos')
+      })
+  }
+
+  useEffect(()=>{
+    document.title = 'Home'
+  },[])
+
+  useEffect(()=>{
+    getRandomAnimals()
+  },[])
 
   return (
     <div className="flex flex-col justify-start items-center bg-gray-200 min-h-screen">
       <Header></Header>
 
       <Banner></Banner>
+
+
+      
+
+      
     </div>
   );
 }
---
+++ app/types/animal.ts
@@ -0,0 +1,15 @@
+interface Animal{
+    id:number
+    name:string
+    description:string
+    inteligence:number
+    height:number
+    weight:number
+    speed:number
+    danger:number
+    longevity:number
+    image:string
+    id_subcategory:number
+    id_diet:number
+
+}
