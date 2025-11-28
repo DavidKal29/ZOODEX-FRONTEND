724ba174172777f47d7020ea7136adaca3895ff0
DavidKal29 <davidkalmuk65@gmail.com>
2025-11-28 16:23:27 +0100

    Modularizamos las tarjetas de los animales en Animal Cards, para que se pueden reutilizar en otros componentes y interfaces

---
+++ app/components/AnimalCard.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Animal} from "../types/animal"
+
+interface AnimalCardProps{
+    animal:Animal
+}
+
+export default function AnimalCard({animal}:AnimalCardProps) {
+  return (
+    <div
+        className="border-2 rounded-2xl overflow-hidden shadow-lg hover:scale-105 transition-transform duration-300 bg-white"
+        style={{ borderColor: animal.color }}
+        >
+        {/* Imagen del animal */}
+        <div className="h-48 w-full overflow-hidden flex justify-center items-center bg-gradient-to-r from-white via-gray-200 to-gray-300 py-4 drop-shadow-md">
+            <img
+            className="h-full object-contain"
+            src={`/animals/${animal.image}`}
+            alt={animal.name}
+            />
+        </div>
+
+        {/* Información del animal */}
+        <div className="p-4 flex flex-col gap-2">
+            <p className="text-gray-500 text-sm">N&#176; {animal.id}</p>
+            <h3 className="text-lg font-semibold">{animal.name}</h3>
+
+            {/* Tipo con color dinámico */}
+            <span
+            className="rounded-full px-2 py-1 text-white text-sm w-fit text-center"
+            style={{ backgroundColor: animal.color }}
+            >
+            {animal.type}
+            </span>
+
+            {/* Altura y peso */}
+            <div className="mt-2 text-gray-700 text-sm flex flex-col gap-1">
+                <p><strong>Altura:</strong> {animal.height} cm</p>
+                <p><strong>Peso:</strong> {animal.weight} kg</p>
+            </div>
+        </div>
+    </div>
+  )
+}
